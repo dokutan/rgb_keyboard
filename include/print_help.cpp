2df1d89d5cf201e47574c4790364a9818e0bd95e
dokutan <54861821+dokutan@users.noreply.github.com>
2020-02-01 23:14:32 +0100

    Add short options to help message

--- include/print_help.cpp
+++ include/print_help.cpp
@@ -24,40 +24,40 @@ void print_help(){
 	"keyboards.\n";
 	std::cout << "Licensed under the GNU GPL version 3 or later.\n\n";
 	std::cout << "Options:\n";
-	std::cout << "\t--help\tDisplays this message\n";
-	std::cout << "\t--list-keys=arg\tList valid keynames (arg: led, map, option)\n\n";
-	std::cout << "\t--color=color\tSets color of the current animation\n";
-	std::cout << "\t--brightness=brightness\tSets brightness of the current animation.\n"
+	std::cout << "\t-h --help\tDisplays this message\n";
+	std::cout << "\t-L --list-keys=arg\tList valid keynames (arg: led, map, option)\n\n";
+	std::cout << "\t-c --color=color\tSets color of the current animation\n";
+	std::cout << "\t-b --brightness=brightness\tSets brightness of the current animation.\n"
 	"\t\tInteger from 0 to 9.\n";
-	std::cout << "\t--speed=speed\tSets speed of the current animation.\n"
+	std::cout << "\t-s --speed=speed\tSets speed of the current animation.\n"
 	"\t\tInteger from 0 to 3.\n";
-	std::cout << "\t--direction=direction\tSets direction of the current animation.\n"
+	std::cout << "\t-t --direction=direction\tSets direction of the current animation.\n"
 	"\t\t\"left\", \"right\", \"up\", \"down\", \"inwards\", \"outwards\".\n\n";
-	std::cout << "\t--fixed\tSets a fixed color\n";
-	std::cout << "\t--sine\tSets a sine wave animation\n";
-	std::cout << "\t--rain\tSets a raindrop animation\n";
-	std::cout << "\t--waterfall\tSets a waterfall animation\n";
-	std::cout << "\t--vortex\tSets a vortex animation\n";
-	std::cout << "\t--swirl\tSets a swirl animation\n";
-	std::cout << "\t--diagonal\tSets a diagonal wave animation\n";
-	std::cout << "\t--breathing\tSets a breathing animation\n";
-	std::cout << "\t--breathing-color\tSets a colorful breathing animation\n";
-	std::cout << "\t--reactive-ripple\tSets a reactive ripple animation\n";
-	std::cout << "\t--reactive-single\tSets a reactive single-key animation\n";
-	std::cout << "\t--reactive-horizontal\tSets a reactive horizontal animation\n";
-	std::cout << "\t--horizontal-wave\tSets a horizontal wave animation\n";
-	std::cout << "\t--vertical-wave\tSets a vertical wave animation\n";
-	std::cout << "\t--pulse\tSets a pulse wave animation\n";
-	std::cout << "\t--hurricane\tSets a hurricane animation\n";
-	std::cout << "\t--ripple\tSets a ripple animation\n";
-	std::cout << "\t--reactive-color=variant\tSets a reactive color animation.\n"
+	std::cout << "\t-f --fixed\tSets a fixed color\n";
+	std::cout << "\t-i --sine\tSets a sine wave animation\n";
+	std::cout << "\t-r --rain\tSets a raindrop animation\n";
+	std::cout << "\t-w --waterfall\tSets a waterfall animation\n";
+	std::cout << "\t-v --vortex\tSets a vortex animation\n";
+	std::cout << "\t-l --swirl\tSets a swirl animation\n";
+	std::cout << "\t-d --diagonal\tSets a diagonal wave animation\n";
+	std::cout << "\t-e --breathing\tSets a breathing animation\n";
+	std::cout << "\t-a --breathing-color\tSets a colorful breathing animation\n";
+	std::cout << "\t-p --reactive-ripple\tSets a reactive ripple animation\n";
+	std::cout << "\t-g --reactive-single\tSets a reactive single-key animation\n";
+	std::cout << "\t-o --reactive-horizontal\tSets a reactive horizontal animation\n";
+	std::cout << "\t-z --horizontal-wave\tSets a horizontal wave animation\n";
+	std::cout << "\t-m --vertical-wave\tSets a vertical wave animation\n";
+	std::cout << "\t-u --pulse\tSets a pulse wave animation\n";
+	std::cout << "\t-n --hurricane\tSets a hurricane animation\n";
+	std::cout << "\t-x --ripple\tSets a ripple animation\n";
+	std::cout << "\t-y --reactive-color=variant\tSets a reactive color animation.\n"
 	"\t\"red\", \"yellow\", \"green\", \"blue\", \"none\" (doesn't change color).\n\n";
-	std::cout << "\t--custom\tEnables custom mode\n";
+	std::cout << "\t-j --custom\tEnables custom mode\n";
 	//std::cout << "\t--custom-clear\tEnables custom mode and clears pattern\n";
-	std::cout << "\t--custom-pattern=file\tSets pattern in custom mode\n";
-	std::cout << "\t--custom-keys=keys\tSets pattern in custom mode\n";
-	std::cout << "\n\t--report-rate=rate\tSets USB report rate (125, 250, 500, 1000) Hz\n";
-	std::cout << "\n\t--keymap=file\tLoad keymap from specified file\n";
+	std::cout << "\t-P --custom-pattern=file\tSets pattern in custom mode\n";
+	std::cout << "\t-K --custom-keys=keys\tSets pattern in custom mode\n";
+	std::cout << "\n\t-R --report-rate=rate\tSets USB report rate (125, 250, 500, 1000) Hz\n";
+	std::cout << "\n\t-M --keymap=file\tLoad keymap from specified file\n";
 	std::cout << "\nColors need to be specified in hexadecimal without "
 	"any prefix.\n\"multi\" can also be used to create a rainbow effect.\n\n";
 }
